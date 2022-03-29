70b15d267832b370a896b1f37d870e3f972c6c89
samuel <1930484upv.edu.mx>
2022-03-29 12:01:48 -0600

    base de datos

---
+++ bd/bd_fd.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE siiupv;
+
+CREATE TABLE alumno(matricula int(7) NOT NULL PRIMARY KEY, nombre varchar(100) NOT NULL, apellido varchar(100) NOT NULL, grupo varchar(50));
+
+CREATE TABLE materia(id varchar(10) NOT NULL PRIMARY KEY, nombre varchar(50) NOT NULL, profesor varchar(100) NOT NULL);
+
+CREATE TABLE profesor(numEmp varchar(20) NOT NULL PRIMARY KEY, nombre varchar(100) NOT NULL, apellido varchar(100) NOT NULL);
+
+ALTER TABLE alumno ADD FOREIGN KEY(grupo) REFERENCES materia(id);
+
+ALTER TABLE materia ADD FOREIGN KEY(profesor) REFERENCES profesor(numEmp);
+
