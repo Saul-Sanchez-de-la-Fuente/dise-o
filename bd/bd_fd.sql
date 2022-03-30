768d930ac7eb3f12e996331458b31dd982dad02e
Samuel-a11 <1930484@upv.edu.mx>
2022-03-30 11:39:47 -0600

    actualizacion de la base de datos

--- bd/bd_fd.sql
+++ bd/bd_fd.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE siiupv;
 
-CREATE TABLE alumno(matricula int(7) NOT NULL PRIMARY KEY, nombre varchar(100) NOT NULL, apellido varchar(100) NOT NULL, grupo varchar(50));
+CREATE TABLE alumno(matricula int(7) NOT NULL PRIMARY KEY, nombre varchar(100) NOT NULL, apellido varchar(100) NOT NULL, grupo varchar(50), correo varchar(18) NOT NULL, contrasena varchar(50) NOT NULL);
 
 CREATE TABLE materia(id varchar(10) NOT NULL PRIMARY KEY, nombre varchar(50) NOT NULL, profesor varchar(100) NOT NULL);
 
